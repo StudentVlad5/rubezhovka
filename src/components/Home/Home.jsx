471c256a3322a83ed4a6182e387fe5508f10a4c4
StudentVlad5 <99014125+StudentVlad5@users.noreply.github.com>
2022-12-29 13:28:41 +0200

    fix menuBar

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,11 +1,26 @@
 import { Outlet} from "react-router-dom";
-import { NavMenu } from 'components/Common/Common'
+import { NavMenu } from 'components/Common/Common';
+// import { StarOutlined, StarFilled, StarTwoTone, WhatsAppOutlined } from '@ant-design/icons';
+import { LeftCircleOutlined, RightCircleOutlined } from '@ant-design/icons';
 import css from "./Home.module.css";
+import { useState } from "react";
 
 export const Home = () => {
+const [statusMenu, setStatusMenu] = useState(false);
+
+const  handleHideBar = () => {
+      document.querySelector('#menuBar').style.display = 'none';
+      setStatusMenu(!statusMenu);
+    }
+const  handleViewBar = () => {
+    document.querySelector('#menuBar').style.display = 'flex';
+    setStatusMenu(!statusMenu);
+    }
+
     return (
-    <>
-        <div className={css.menu}>
+    <> {statusMenu && <RightCircleOutlined className={css.rightArrow} onClick={handleViewBar}/>}
+        <div className={css.menu} id="menuBar">
+            <LeftCircleOutlined className={css.leftArrow} onClick={handleHideBar}/>
             <h3>Михайлівка-Рубежівка</h3>
             {NavMenu("bus_way_shadule", <h5>Маршрути автобусів</h5>)}
             {NavMenu("weather", <h5>Погода</h5>)}
